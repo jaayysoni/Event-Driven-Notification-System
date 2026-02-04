aa71306e18a49804b53dadb7d0e08bd17311f84f
JayySoni <sonijaysoni459@gmail.com>
2026-02-04 21:28:58 +0530

    Added UI and new pacages

---
+++ alembic/env.py
@@ -0,0 +1,4 @@
+from app.database import Base
+from app import models
+
+target_metadata = Base.metadata
--- app/database.py
+++ app/database.py
@@ -1,10 +1,8 @@
-from sqlalchemy import create_engine # type: ignore
-from sqlalchemy.orm import sessionmaker, declarative_base #type: ignore
-import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from app.config import settings
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:password@localhost:5432/notifications")
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
